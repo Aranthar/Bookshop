9f35f28aa92a2f18f3fd1e480b91c67b5ed79a98
Aranthar <dvkolomiets@gmail.com>
2024-10-27 19:54:58 +0500

    Write ViewModels with Retrofit

--- app/src/main/java/ru/bookshop/ui/screens/books/presentation/BooksViewModel.kt
+++ app/src/main/java/ru/bookshop/ui/screens/books/presentation/BooksViewModel.kt
@@ -1,7 +1,17 @@
 package ru.bookshop.ui.screens.books.presentation
 
+import android.util.Log
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
 import ru.bookshop.data.models.BookDTO
+import ru.bookshop.repository.RepositoryInstance
 import ru.bookshop.ui.common.models.BaseViewModel
 import ru.bookshop.ui.screens.books.models.BooksEvent
 import ru.bookshop.ui.screens.books.models.BooksState
@@ -11,5 +21,24 @@ import javax.inject.Inject
 class BooksViewModel @Inject constructor() :
     BaseViewModel<BooksState, BooksEvent>(initialState = BooksState()) {
 
-    private var booksList: List<BookDTO>? = listOf()
+    private val _books = MutableStateFlow<List<BookDTO>>(emptyList())
+    val books: StateFlow<List<BookDTO>> = _books.asStateFlow()
+
+    fun fetchBooks() {
+        viewModelScope.launch {
+            RepositoryInstance.api.getBooks()
+                .flowOn(Dispatchers.IO)
+                .catch { e ->
+                    _books.value = listOf(BookDTO(
+                        id = -1,
+                        title = e.toString(),
+                        imageId = 0,
+                    ))
+                    Log.e("Retrofit", e.toString())
+                }
+                .collect { bookList ->
+                    _books.value = bookList
+                }
+        }
+    }
 }
--- app/src/main/java/ru/bookshop/ui/screens/details/presentation/DetailsViewModel.kt
+++ app/src/main/java/ru/bookshop/ui/screens/details/presentation/DetailsViewModel.kt
@@ -1,6 +1,17 @@
 package ru.bookshop.ui.screens.details.presentation
 
+import android.util.Log
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+import ru.bookshop.data.models.CharacteristicsDTO
+import ru.bookshop.repository.RepositoryInstance
 import ru.bookshop.ui.common.models.BaseViewModel
 import ru.bookshop.ui.screens.details.models.DetailsEvent
 import ru.bookshop.ui.screens.details.models.DetailsState
@@ -10,4 +21,20 @@ import javax.inject.Inject
 class DetailsViewModel @Inject constructor() :
     BaseViewModel<DetailsState, DetailsEvent>(initialState = DetailsState()) {
 
+    private val _bookDetails = MutableStateFlow<CharacteristicsDTO?>(null)
+    val bookDetails: StateFlow<CharacteristicsDTO?> = _bookDetails.asStateFlow()
+
+    fun fetchBookDetails(bookId: Int) {
+        viewModelScope.launch {
+            RepositoryInstance.api.getBookDetails(bookId)
+                .flowOn(Dispatchers.IO)
+                .catch { e ->
+                    _bookDetails.value = null
+                    Log.e("Retrofit", e.toString())
+                }
+                .collect { book ->
+                    _bookDetails.value = book
+                }
+        }
+    }
 }
